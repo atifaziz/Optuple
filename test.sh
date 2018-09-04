0be5fce9dd07b97d77b8ac86ca4910d8fe82f8c6
Atif Aziz <code@raboof.com>
2018-09-04 17:51:53 +0200

    Use opencover format for coverage report

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 [[ -e test.sh ]] || { echo >&2 "Please cd into the script location before running it."; exit 1; }
 set -e
 ./build.sh
-dotnet test --no-build tests -c Debug -p:CollectCoverage=true -p:Exclude=[NUnit*]*
+dotnet test --no-build tests -c Debug -p:CollectCoverage=true \
+                                      -p:CoverletOutputFormat=opencover \
+                                      -p:Exclude=[NUnit*]*
 dotnet test --no-build tests -c Release
 
