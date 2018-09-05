1fa9cd735601bf40b82d15dc3a4883caf951112d
Atif Aziz <code@raboof.com>
2018-09-05 16:36:33 +0200

    Remove unused imports

--- tests/OptionalTests.cs
+++ tests/OptionalTests.cs
@@ -2,10 +2,8 @@ namespace JustNothing.Tests
 {
     using System;
     using System.Collections.Generic;
-    using System.Text;
     using System.Linq;
     using NUnit.Framework;
-    using Case = Option.Case;
     using JustNothing.Linq;
 
     [TestFixture]
