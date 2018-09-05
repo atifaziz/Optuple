1cf73d64ce665e987c775ececd2b663fff072a16
Adrian Guerra <adriman02@live.com>
2018-09-05 13:42:34 +0200

    Initial commit

---
+++ tests/OptionalTests.cs
@@ -0,0 +1,16 @@
+namespace JustNothing.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Linq;
+    using NUnit.Framework;
+    using Case = Option.Case;
+    using JustNothing.Linq;
+
+    [TestFixture]
+    class OptionalTests
+    {
+        
+    }
+}
