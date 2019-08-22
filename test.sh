2831c97b2e516caa2d12613b43644d0575e94701
Atif Aziz <code@raboof.com>
2019-08-22 10:42:33 +0200

    Improve cd-ing in bash scripts

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-[[ -e test.sh ]] || { echo >&2 "Please cd into the script location before running it."; exit 1; }
 set -e
+cd "$(dirname "$0")"
 ./build.sh
 dotnet test --no-build tests -c Debug -p:CollectCoverage=true \
                                       -p:CoverletOutputFormat=opencover \
